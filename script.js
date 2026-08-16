aec9c21318a784bc86e01b9f6684686a6b343fde
1kaylin <138742638+1kaylin@users.noreply.github.com>
2026-08-16 00:41:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -47,11 +47,11 @@ function onYouTubeIframeAPIReady() {
             onReady: function(event) {
                 ready = true;
 
-                event.target.mute();
                 event.target.setVolume(Number(volume.value));
+                event.target.unMute();
                 event.target.playVideo();
 
-                muteIcon.textContent = '🔇';
+                muteIcon.textContent = '🔊';
                 playIcon.textContent = '❚❚';
             },
 
